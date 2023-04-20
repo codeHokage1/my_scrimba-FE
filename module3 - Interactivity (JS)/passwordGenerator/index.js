1c2857072e74d8a69906fd4b6bbec73f671f1156
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-04-20 12:53:42 +0100

    Add a feature to allow user choose a length

--- module3 - Interactivity (JS)/passwordGenerator/index.js
+++ module3 - Interactivity (JS)/passwordGenerator/index.js
@@ -1,24 +1,36 @@
 const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
 
+const advanceOption = document.getElementById("advanced-option");
+const options = document.getElementById("options");
+const passwordLength = document.getElementById("password-length");
+
 const generateBtn = document.getElementById("generate-password");
 
 const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "~`!@#$%^&*()_-+={[}]|:;,<.>/?";
 
-const generatePassword = () => {
+const generatePassword = (length = 15) => {
 	let pool = alphabets + numbers + symbols;
 	let password = "";
-   for (let i = 0; i < 15; i++) {
-      password += pool[Math.floor(Math.random() * pool.length)];
-   }
-   return password;
+	for (let i = 0; i < length; i++) {
+		password += pool[Math.floor(Math.random() * pool.length)];
+	}
+	return password;
 };
 
 const renderPassword = () => {
-   password1.textContent = generatePassword();
-   password2.textContent = generatePassword();
-}
+	password1.textContent = passwordLength.value
+		? generatePassword(passwordLength.value)
+		: generatePassword();
+	password2.textContent = passwordLength.value
+		? generatePassword(passwordLength.value)
+		: generatePassword();
+};
 
 generateBtn.addEventListener("click", renderPassword);
+
+advanceOption.addEventListener("click", () => {
+	options.classList.toggle("options");
+});
