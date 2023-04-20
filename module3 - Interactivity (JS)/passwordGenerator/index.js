d94992a34ab58381a3f6fe5f7ca22f67406d3db1
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-04-20 14:22:41 +0100

    Add copy to clipboard feature

--- module3 - Interactivity (JS)/passwordGenerator/index.js
+++ module3 - Interactivity (JS)/passwordGenerator/index.js
@@ -4,8 +4,16 @@ const password2 = document.getElementById("password2");
 const advanceOption = document.getElementById("advanced-option");
 const options = document.getElementById("options");
 const passwordLength = document.getElementById("password-length");
+const error = document.getElementById("error");
 
 const generateBtn = document.getElementById("generate-password");
+
+const clipBoard1 = document.getElementById("clipboard1");
+const clipBoard2 = document.getElementById("clipboard2");
+const copyInfo1 = document.getElementById("copy-info1")
+const copyInfo2 = document.getElementById("copy-info2")
+
+
 
 const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
@@ -21,16 +29,62 @@ const generatePassword = (length = 15) => {
 };
 
 const renderPassword = () => {
-	password1.textContent = passwordLength.value
-		? generatePassword(passwordLength.value)
-		: generatePassword();
-	password2.textContent = passwordLength.value
-		? generatePassword(passwordLength.value)
-		: generatePassword();
+	if (!passwordLength.value) {
+		password1.textContent = generatePassword();
+		password2.textContent = generatePassword();
+	} else if (passwordLength.value && passwordLength.value < 8) {
+		error.textContent = "Password length must be greater than 8";
+		setTimeout(() => {
+			error.textContent = "";
+		}, 3000);
+	} else {
+		password1.textContent = generatePassword(passwordLength.value);
+		password2.textContent = generatePassword(passwordLength.value);
+	}
 };
 
 generateBtn.addEventListener("click", renderPassword);
 
 advanceOption.addEventListener("click", () => {
 	options.classList.toggle("options");
+});
+
+
+
+clipBoard1.addEventListener("click", () => {
+	const textarea = document.createElement("textarea");
+   textarea.value = password1.textContent;
+
+	// Add the textarea element to the page
+	document.body.appendChild(textarea);
+
+	// Copy the text from the textarea to the clipboard
+	textarea.select();
+	document.execCommand("copy");
+
+	// Remove the textarea element from the page
+   document.body.removeChild(textarea);
+   copyInfo1.textContent = "Password copied to clipboard!"
+   setTimeout(() => {
+      copyInfo1.textContent = ""
+   }, 5000)
+});
+
+clipBoard2.addEventListener("click", () => {
+	const textarea = document.createElement("textarea");
+   textarea.value = password2.textContent;
+
+	// Add the textarea element to the page
+	document.body.appendChild(textarea);
+
+	// Copy the text from the textarea to the clipboard
+	textarea.select();
+	document.execCommand("copy");
+
+	// Remove the textarea element from the page
+   document.body.removeChild(textarea);
+   copyInfo2.textContent = "Password copied to clipboard!"
+   setTimeout(() => {
+      copyInfo2.textContent = ""
+   }, 5000)
 });
