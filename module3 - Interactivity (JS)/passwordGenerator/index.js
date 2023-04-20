a9a31a0266cfc43ae1604ea07a5ac64145fd2df3
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-04-20 15:14:44 +0100

    Add feature to select whether or not to include numbers and/or symbols

--- module3 - Interactivity (JS)/passwordGenerator/index.js
+++ module3 - Interactivity (JS)/passwordGenerator/index.js
@@ -10,17 +10,37 @@ const generateBtn = document.getElementById("generate-password");
 
 const clipBoard1 = document.getElementById("clipboard1");
 const clipBoard2 = document.getElementById("clipboard2");
-const copyInfo1 = document.getElementById("copy-info1")
-const copyInfo2 = document.getElementById("copy-info2")
+const copyInfo1 = document.getElementById("copy-info1");
+const copyInfo2 = document.getElementById("copy-info2");
 
-
+const addNumbers = document.getElementById("add-numbers");
+const addSymbols = document.getElementById("add-symbols");
 
 const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "~`!@#$%^&*()_-+={[}]|:;,<.>/?";
 
+let numbersWithPassword, symbolsWithPassword;
+addNumbers.addEventListener("change", () => {
+	numbersWithPassword = addNumbers.checked;
+});
+addSymbols.addEventListener("change", () => {
+	symbolsWithPassword = addSymbols.checked;
+});
+
 const generatePassword = (length = 15) => {
-	let pool = alphabets + numbers + symbols;
+	let pool;
+	if (
+		(numbersWithPassword && symbolsWithPassword) ||
+		(!numbersWithPassword && !symbolsWithPassword)
+	) {
+		pool = alphabets + numbers + symbols;
+	} else if (numbersWithPassword) {
+		pool = alphabets + numbers;
+	} else if (symbolsWithPassword) {
+		pool = alphabets + symbols;
+	}
+
 	let password = "";
 	for (let i = 0; i < length; i++) {
 		password += pool[Math.floor(Math.random() * pool.length)];
@@ -49,11 +69,18 @@ advanceOption.addEventListener("click", () => {
 	options.classList.toggle("options");
 });
 
-
-
 clipBoard1.addEventListener("click", () => {
+	if (!password1.innerText) {
+		copyInfo1.classList.add("copy-info-error");
+		copyInfo1.textContent = "Generate a password first!";
+		setTimeout(() => {
+			copyInfo1.textContent = "";
+			copyInfo1.classList.remove("copy-info-error");
+		}, 5000);
+		return;
+	}
 	const textarea = document.createElement("textarea");
-   textarea.value = password1.textContent;
+	textarea.value = password1.textContent;
 
 	// Add the textarea element to the page
 	document.body.appendChild(textarea);
@@ -63,16 +90,25 @@ clipBoard1.addEventListener("click", () => {
 	document.execCommand("copy");
 
 	// Remove the textarea element from the page
-   document.body.removeChild(textarea);
-   copyInfo1.textContent = "Password copied to clipboard!"
-   setTimeout(() => {
-      copyInfo1.textContent = ""
-   }, 5000)
+	document.body.removeChild(textarea);
+	copyInfo1.textContent = "Password copied to clipboard!";
+	setTimeout(() => {
+		copyInfo1.textContent = "";
+	}, 5000);
 });
 
 clipBoard2.addEventListener("click", () => {
+	if (!password2.innerText) {
+		copyInfo2.classList.add("copy-info-error");
+		copyInfo2.textContent = "Generate a password first!";
+		setTimeout(() => {
+			copyInfo2.textContent = "";
+			copyInfo2.classList.remove("copy-info-error");
+		}, 5000);
+		return;
+	}
 	const textarea = document.createElement("textarea");
-   textarea.value = password2.textContent;
+	textarea.value = password2.textContent;
 
 	// Add the textarea element to the page
 	document.body.appendChild(textarea);
@@ -82,9 +118,9 @@ clipBoard2.addEventListener("click", () => {
 	document.execCommand("copy");
 
 	// Remove the textarea element from the page
-   document.body.removeChild(textarea);
-   copyInfo2.textContent = "Password copied to clipboard!"
-   setTimeout(() => {
-      copyInfo2.textContent = ""
-   }, 5000)
+	document.body.removeChild(textarea);
+	copyInfo2.textContent = "Password copied to clipboard!";
+	setTimeout(() => {
+		copyInfo2.textContent = "";
+	}, 5000);
 });
