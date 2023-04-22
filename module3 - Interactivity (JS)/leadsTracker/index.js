a53220930f273c547e433d5f284d47d1f7cf6115
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-04-22 21:02:19 +0100

    Add deleted function and error message

--- module3 - Interactivity (JS)/leadsTracker/index.js
+++ module3 - Interactivity (JS)/leadsTracker/index.js
@@ -1,22 +1,39 @@
 const urlInput = document.getElementById('url-input');
 const saveInputBtn = document.getElementById('save-input');
+const deleteBtn = document.getElementById('delete');
+
+const inputInfo = document.getElementById('info');
 
 let savedItems = document.getElementById('saved-items');
 
-const urls = JSON.parse(localStorage.getItem('urls')) || [];
+let urls = JSON.parse(localStorage.getItem('urls')) || [];
 
 const renderSavedItems = () => {
    savedItems.innerHTML = '';
    for(let i = 0; i < urls.length; i++) {
-      const li = document.createElement('li');
-      li.textContent = urls[i];
-      savedItems.appendChild(li);
+      const li = `<li><a href=${urls[i]} target="_blank">${urls[i]}</a></li>`;
+      savedItems.innerHTML += li;
    }
 }
-renderSavedItems()
+renderSavedItems();
+
+const saveToLocalStorage = () => {
+   localStorage.setItem('urls', JSON.stringify(urls));
+}
+
+const displayError = (message) => {
+   inputInfo.textContent = message;
+   setTimeout(() => {
+      inputInfo.textContent = ""
+   }, 3000)
+}
 
 const saveInput = () => {
    const url = urlInput.value;
+   if (!url) {
+      displayError("You have to input a URL!");
+      return;
+   }
    urls.push(url);
    console.log(urls);
    urlInput.value = '';
@@ -24,8 +41,13 @@ const saveInput = () => {
    saveToLocalStorage();
 }
 
-const saveToLocalStorage = () => {
-   localStorage.setItem('urls', JSON.stringify(urls));
+const deleteUrls = () => {
+   localStorage.clear();
+   urls = [];
+   renderSavedItems();
 }
 
+
+
 saveInputBtn.addEventListener('click', saveInput);
+deleteBtn.addEventListener('click', deleteUrls);
