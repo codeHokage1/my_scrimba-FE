1b49489ec05eb47d2071ef2d3a8ffc0f5d9326f6
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-04-26 23:25:37 +0100

    Render all posts of oldagram with JS

--- module4 - essential css/oldagram/index.js
+++ module4 - essential css/oldagram/index.js
@@ -1,30 +1,66 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+const allPosts = document.querySelector("#all-posts");
 
+const posts = [
+	{
+		name: "Vincent van Gogh",
+		username: "vincey1853",
+		location: "Zundert, Netherlands",
+		avatar: "images/avatar-vangogh.jpg",
+		post: "images/post-vangogh.jpg",
+		comment: "just took a few mushrooms lol",
+		likes: 21,
+	},
+	{
+		name: "Gustave Courbet",
+		username: "gus1819",
+		location: "Ornans, France",
+		avatar: "images/avatar-courbet.jpg",
+		post: "images/post-courbet.jpg",
+		comment: "i'm feelin a bit stressed tbh",
+		likes: 4,
+	},
+	{
+		name: "Joseph Ducreux",
+		username: "jd1735",
+		location: "Paris, France",
+		avatar: "images/avatar-ducreux.jpg",
+		post: "images/post-ducreux.jpg",
+		comment:
+			"gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+		likes: 152,
+	},
+];
+
+const renderPosts = () => {
+	for (let post of posts) {
+		let newPost = `
+        <section class="post">
+			<div class="post-user">
+				<img src="${post.avatar}" alt="user-icon" />
+				<div>
+					<h3 class="post-username">${post.name}</h3>
+					<p class="post-location">${post.location}</p>
+				</div>
+			</div>
+			<img
+				src="${post.post}"
+				alt="painting of ${post.name}"
+				class="post-image"
+			/>
+			<div class="post-actions">
+				<img src="images/icon-heart.png" alt="heart icon" />
+				<img src="images/icon-comment.png" alt="comment icon" />
+				<img src="images/icon-dm.png" alt="share icon" />
+			</div>
+			<p class="likes">
+				<span id="likes-number">${post.likes}</span> likes
+			</p>
+			<p class="caption">
+				<span class="user-caption">${post.username}</span> j${post.comment}
+		</section>
+        `;
+        allPosts.innerHTML += newPost;
+	}
+};
+
+renderPosts();
