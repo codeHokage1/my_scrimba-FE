de3b0e39350c51adc8f0f97ec05d372bda0466c1
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-05-03 22:44:04 +0100

    Display all tweets available and the textarea

--- module5 - essential JS/twimba/index.js
+++ module5 - essential JS/twimba/index.js
@@ -1,58 +1,42 @@
-const tweetsData = [   
-    {
-        handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
-        likes: 27,
-        retweets: 10,
-        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
-            Guaranteed return on investment. HMU DMs open!!`,
-        replies: [],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
-    },    
-    {
-        handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
-        likes: 6500,
-        retweets: 234,
-        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
-        replies: [
-                  {
-                handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
-                tweetText: `Yes! Sign me up! 😎🛩`,
-            },
-                  {
-                handle: `@ChuckNorris ✅`,
-                profilePic: `images/chucknorris.jpeg`,
-                tweetText: `I went last year😴`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
-    },
-        {
-        handle: `@NoobCoder12`,
-        profilePic: `images/flower.png`,
-        likes: 10,
-        retweets: 3,
-        tweetText: `Are you a coder if you only know HTML?`,
-        replies: [
-            {
-                handle: `@StackOverflower ☣️`,
-                profilePic: `images/overflow.png`,
-                tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
-            },
-            {
-                handle: `@YummyCoder64`,
-                profilePic: `images/love.png`,
-                tweetText: `You are wonderful just as you are! ❤️`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
-    },     
-]
+import { tweetsData } from "./data.js";
+
+const tweetBtn = document.querySelector("#tweet-btn");
+const tweetInput = document.querySelector("#tweet-input");
+
+const feed = document.querySelector("#feed");
+
+tweetBtn.addEventListener("click", () => {
+	window.alert(tweetInput.value);
+	tweetInput.value = "";
+});
+
+const renderTweet = (tweets) => {
+	feed.innerHTML = "";
+
+	tweets.forEach((tweet) => {
+		feed.innerHTML += `
+                <div class="tweet" id="${tweet.uuid}">
+                    <div class="tweet-inner">
+                        <img src="${tweet.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${tweet.handle}</p>
+                            <p class="tweet-text">${tweet.tweetText}</p>
+                            <div class="tweet-details">
+                                <span class="tweet-detail">
+                                    ${tweet.replies.length}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.likes}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.retweets}
+                                </span>
+                            </div>   
+                        </div>            
+                    </div>
+                </div>
+        `;
+	});
+};
+
+renderTweet(tweetsData)
