9734eae425ede8d180ca03d828e677b0b7cab89e
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-05-24 21:49:44 +0100

    Add script and add feature

--- module3 - Interactivity (JS)/addToCart/index.js
+++ module3 - Interactivity (JS)/addToCart/index.js
@@ -1,27 +1,47 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
+import {
+	getDatabase,
+	ref,
+	push,
+	onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSetting = {
-   databaseURL: "https://addtocart-61a59-default-rtdb.firebaseio.com/"
-}
+	databaseURL: "https://addtocart-61a59-default-rtdb.firebaseio.com/",
+};
 
 const app = initializeApp(appSetting);
 const db = getDatabase(app);
-const itemsInCart = ref(db, 'itemsInCart');
+const itemsInCart = ref(db, "itemsInCart");
 
-console.log(app);
-const inputField = document.querySelector('#input-field');
-const addBtn = document.querySelector('#add-button');
+const inputField = document.querySelector("#input-field");
+const addBtn = document.querySelector("#add-button");
 
-const saveItem = async (item) => {
-   await push(itemsInCart, item);
-   alert(`You added ${item} to your cart!`);
-}
+const itemsList = document.querySelector(".itemsList");
 
-addBtn.addEventListener('click', async() => {
-   if (inputField.value) {
-      await saveItem(inputField.value);
-      inputField.value = '';
-   }
-})
+const fetchItems = async () => {
+   itemsList.innerHTML = "";
+	await onValue(itemsInCart, function (snapshot) {
+		let dbItems = Object.values(snapshot.val());
+		let items = ''
+		dbItems.forEach((item) => {
+			items += `<li>${item}</li>`;
+      });
+      itemsList.innerHTML = items;
+   });
+
+};
+fetchItems();
+
+const addItem = async (listItem) => {
+	await push(itemsInCart, listItem);
+	// await fetchItems();
+
+	inputField.value = "";
+};
+
+addBtn.addEventListener("click", async () => {
+	if (inputField.value) {
+		await addItem(inputField.value);
+	}
+});
