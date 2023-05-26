4ad701fc669a2c971aa1ab35045a84774a013213
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-05-27 00:03:56 +0100

    Complete stretch goal p1: new interface

--- module3 - Interactivity (JS)/champions/index.js
+++ module3 - Interactivity (JS)/champions/index.js
@@ -17,18 +17,49 @@ const endorsements = ref(db, "endorsements");
 
 const endorsementsDIV = document.querySelector('.endorsements');
 const entry = document.querySelector('#comment');
+const fromVal = document.querySelector('.from');
+const toVal = document.querySelector('.to');
+
 
 const publishBtn = document.querySelector('.publish');
 
 publishBtn.addEventListener('click', () => {
-   if (!entry.value) return;
-   push(endorsements, entry.value);
-   const newEndorsement = `
-      <div class="endorsement">
-         <p>${entry.value}</p>
-      </div>
-   `
-   endorsementsDIV.innerHTML += newEndorsement;
+   if (!entry.value || !toVal.value) return;
+   let sender = fromVal.value || 'Anonymous';
+   let endorsement = {
+      to: toVal.value,
+      endorsement: entry.value,
+      from: sender
+   }
+
+   push(endorsements, endorsement);
+   
    entry.value = '';
+   fromVal.value = '';
+   toVal.value = '';
 })
+
+function fetchEndorsements() {
+   onValue(endorsements, (snapshot) => {
+      if (!snapshot.exists()) {
+         endorsementsDIV.innerHTML = '<p style="color: white; font-size: 1.2rem">No endorsements yet</p>';
+         return;
+      }
+      const data = snapshot.val();
+      console.log(Object.entries(data))
+      const endorsements = Object.values(data);
+      const endorsementsHTML = endorsements.map(endorsement => {
+         return `
+            <div class="endorsement">
+               <h3>To: ${endorsement.to}</h3>
+               <p>${endorsement.endorsement}</p>
+               <h3>From: ${endorsement.from}</h3>
+            </div>
+         `
+      }).join('');
+      endorsementsDIV.innerHTML = endorsementsHTML;
+   })
+}
+
+fetchEndorsements()
 
