2b7765ef0d49b843f8e021b61886d2ee426fe151
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-06-03 22:20:35 +0100

    Refactor code

--- module7 - nextLevelJS/gameApp/index.js
+++ module7 - nextLevelJS/gameApp/index.js
@@ -1,44 +1,49 @@
-const hero = {
-   id: 'hero',
-   name: 'Wizard',
-   avatar: 'images/wizard.png',
-   health: 60,
-   diceRoll: [],
-   diceCount: 3
+function Character(data) {
+	Object.assign(this, data);
+	this.generateHTML = function () {
+		document.getElementById(this.id).innerHTML = `
+      <div class="character-card">
+        <h4 class="name"> ${this.name} </h4>
+        <img class="avatar" src="${this.avatar}"/>
+        <p class="health">health: <b> ${this.health} </b></p>
+        <div class="dice-container">${this.diceRoll
+					.map((dice) => `<div class="dice"> ${dice} </div>`)
+					.join("")}</div>
+      </div>`;
+	};
+	this.rollDice = function () {
+      this.diceRoll = new Array(this.diceCount).fill(0).map(() => Math.ceil(Math.random() * 6));
+      this.generateHTML()
+   };
 }
+const data1 = {
+	id: "hero",
+	name: "Wizard",
+	avatar: "images/wizard.png",
+	health: 60,
+	diceRoll: [],
+	diceCount: 3,
+};
 
-const monster = {
-   id: 'monster',
-   name: 'Orc',
-   avatar: 'images/orc.png',
-   health: 10,
-   diceRoll: [],
-   diceCount: 1
-}
+const data2 = {
+	id: "monster",
+	name: "Orc",
+	avatar: "images/orc.png",
+	health: 10,
+	diceRoll: [],
+	diceCount: 1,
+};
 
-function renderCharacter({id, name, avatar, health, diceRoll, diceCount}){
-  document.getElementById(id).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">${diceRoll.map(dice => `<div class="dice"> ${dice} </div>`).join('')}</div>
-    </div>`;   
-}
+const hero = new Character(data1);
+const monster = new Character(data2);
 
-renderCharacter(hero);
-renderCharacter(monster);
 
-function rollDice (diceCount) {
-  return new Array(diceCount).fill(0).map(dice => Math.ceil(Math.random() * 6));
-}
+hero.generateHTML();
+monster.generateHTML();
 
 function attack() {
-   hero.diceRoll = rollDice(hero.diceCount);
-   monster.diceRoll = rollDice(monster.diceCount);
-
-   renderCharacter(hero);
-   renderCharacter(monster);
+	hero.rollDice();
+	monster.rollDice();
 }
 
-document.getElementById('attack-button').addEventListener('click', attack);
+document.getElementById("attack-button").addEventListener("click", attack);
